67bf706d0af51cc37f30b61e53634b5b4ed09fc9
Himanshu Rijal <himanshurijal@Himanshus-MacBook-Pro.local>
2021-02-26 22:12:03 -0600

    Add functionality to disable Tweet button when character count is exceeded

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -27,6 +30,7 @@ public class ComposeActivity extends AppCompatActivity {
 
     // VIEW
     EditText etCompose;
+    TextView tvCharCounter;
     Button btnTweet;
 
     // ADAPTER
@@ -42,7 +46,36 @@ public class ComposeActivity extends AppCompatActivity {
 
         // VIEW
         etCompose = findViewById(R.id.etCompose);
+        tvCharCounter = findViewById(R.id.tvCharCounter);
         btnTweet = findViewById(R.id.btnTweet);
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not needed
+            }
+
+            // Disable "Tweet" button if character count is
+            // more than 280
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int tweetLength = etCompose.getText().toString().length();
+                String tweetLengthInString = String.valueOf(etCompose.getText().toString().length());
+                tvCharCounter.setText(tweetLengthInString + "/280");
+
+                if(tweetLength > 280) {
+                    btnTweet.setEnabled(false);
+                }
+                else {
+                    btnTweet.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Not needed
+            }
+        });
 
         // Make API call to Twitter to post the Tweet.
         btnTweet.setOnClickListener(new View.OnClickListener() {
@@ -54,7 +87,7 @@ public class ComposeActivity extends AppCompatActivity {
                             "Tweet cannot be empty.", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                else if(tweetContent.length() > 140) {
+                else if(tweetContent.length() > 280) {
                     Toast.makeText(ComposeActivity.this,
                             "Tweet is too long.", Toast.LENGTH_SHORT).show();
                     return;
